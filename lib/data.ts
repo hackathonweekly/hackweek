0ee15e5a8fe1b0526aaff5c46142a040943e1e96
jackiexiao <707610215@qq.com>
2024-12-24 18:25:15 +0800

    fix mbti color

--- lib/data.ts
+++ lib/data.ts
@@ -18,7 +18,7 @@ export const members: Member[] = [
     "mbti": "INTJ",
     "hobbies": ["骑行", "摄影"],
     "avatar": "/christmas/image_1.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "前科创学院创业者，现某AI+硬件公司产品经理，关注需求的非典型理工男"
   },
   {
@@ -29,7 +29,7 @@ export const members: Member[] = [
     "zodiac": "射手座",
     "hobbies": ["科技", "AI", "炒股", "体育", "音乐", "美女"],
     "avatar": "/christmas/image_2.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "在一家体育科技互联网公司做产品经理"
   },
   {
@@ -39,7 +39,7 @@ export const members: Member[] = [
     "zodiac": "巨蟹座",
     "hobbies": ["旅行", "美食", "Coding"],
     "avatar": "/christmas/image_3.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "目前就职于Sea-Shopee,正在学习web3前端（偏全栈），准备转行web3中"
   },
   {
@@ -50,7 +50,7 @@ export const members: Member[] = [
     "zodiac": "摩羯座",
     "hobbies": ["绘画", "音乐", "健身", "游泳"],
     "avatar": "/christmas/image_4.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "擅长AIGC内容营销，目前主推微软openai大模型，做混合云"
   },
   {
@@ -60,7 +60,7 @@ export const members: Member[] = [
     "mbti": "INTJ",
     "hobbies": ["读书"],
     "avatar": "/christmas/image_5.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "制造业隐形冠军AI应用专员"
   },
   {
@@ -71,7 +71,7 @@ export const members: Member[] = [
     "zodiac": "双子",
     "hobbies": ["阅读"],
     "avatar": "/christmas/image_6.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "网络服务器运维、独立开发者、图书管理员、80后"
   },
   {
@@ -81,7 +81,7 @@ export const members: Member[] = [
     "zodiac": "天蝎座",
     "hobbies": ["阅读", "聊天", "时尚穿搭"],
     "avatar": "/christmas/image_7.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "喜欢到处探险的好奇星人，目前是服装公司的商品管理，GSC深圳社区的 shaper，最近关注身心健康议题"
   },
   {
@@ -92,7 +92,7 @@ export const members: Member[] = [
     "zodiac": "狮子座",
     "hobbies": ["推理小说", "数学", "刺激事物"],
     "avatar": "/christmas/image_8.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "推理小说爱好者，喜欢做数学，喜欢刺激事物"
   },
   {
@@ -114,7 +114,7 @@ export const members: Member[] = [
     "zodiac": "水瓶",
     "hobbies": ["游泳", "跑步", "研究和做AI类项目"],
     "avatar": "/christmas/image_10.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "研究和做AI类项目"
   },
   {
@@ -124,7 +124,7 @@ export const members: Member[] = [
     "zodiac": "水瓶",
     "hobbies": ["编程"],
     "avatar": "/christmas/image_11.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "华为鸿蒙外包软件工程师"
   },
   {
@@ -135,7 +135,7 @@ export const members: Member[] = [
     "zodiac": "射手座",
     "hobbies": ["旅行"],
     "avatar": "/christmas/image_12.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "真诚待人，积极乐观"
   },
   {
@@ -146,7 +146,7 @@ export const members: Member[] = [
     "zodiac": "天蝎座",
     "hobbies": ["网球", "健身", "徒步"],
     "avatar": "/christmas/image_13.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "后续补充"
   },
   {
@@ -156,7 +156,7 @@ export const members: Member[] = [
     "zodiac": "天蝎座",
     "hobbies": ["电影", "小说"],
     "avatar": "/christmas/image_14.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "技术全栈，小团队创业者"
   },
   {
@@ -167,7 +167,7 @@ export const members: Member[] = [
     "zodiac": "水瓶座",
     "hobbies": ["写作", "画画", "健身"],
     "avatar": "/christmas/image_15.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "目前互联网行业运营从业者，打算转码/产品，坚持走出舒适区，扩大成长区，长期主义者"
   },
   {
@@ -177,7 +177,7 @@ export const members: Member[] = [
     "mbti": "ENTJ",
     "hobbies": ["SpaceX"],
     "avatar": "/christmas/image_16.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "AI.数字人.区块链"
   },
   {
@@ -187,7 +187,7 @@ export const members: Member[] = [
     "mbti": "INFJ",
     "hobbies": ["摸鱼"],
     "avatar": "/christmas/image_17.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "AI.数字人.区块链.动手能力强"
   },
   {
@@ -198,7 +198,7 @@ export const members: Member[] = [
     "zodiac": "天蝎座",
     "hobbies": ["旅行"],
     "avatar": "/christmas/image_18.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "混合现实和先进人机交互领域的创业者"
   },
   {
@@ -218,7 +218,7 @@ export const members: Member[] = [
     "zodiac": "巨蟹座",
     "hobbies": ["脱口秀", "旅行", "撸猫/狗"],
     "avatar": "/christmas/image_20.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "前雷鸟科技海外产品经理、度小满AI产品经理，宇伴科技（女性AI情感陪伴）CEO&Founder"
   },
   {
@@ -228,7 +228,7 @@ export const members: Member[] = [
     "mbti": "ENTP",
     "hobbies": ["旅游"],
     "avatar": "/christmas/image_21.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "来自普火科技有限公司，业务有做软件外包以及ai相关的产品。目前自研产品是ai获客和ai销售系统"
   },
   {
@@ -239,7 +239,7 @@ export const members: Member[] = [
     "zodiac": "天平",
     "hobbies": ["桌游", "阅读", "足球"],
     "avatar": "/christmas/image_22.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "前Tiktok PM，Mutuavox的创始人，当前在研发一款Content Repurposing Application（WUI.AI），主要是为全球个人创作者将文字，音频和视频内容自动调整为适合YouTube、TikTok和Reels等平台的长短视频并做自动发布"
   },
   {
@@ -261,7 +261,7 @@ export const members: Member[] = [
     "zodiac": "处女座",
     "hobbies": ["brain orgasm", "哲学", "兜风", "骑马", "阅读", "跳舞", "唱歌", "中英演讲辩论", "策略类桌游", "AI应用", "自由泳", "看电影美剧", "做料理", "撸铁"],
     "avatar": "/christmas/image_24.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "1.0经验从好未来课程开发转行转岗到产品经理，产品经理（赛道：金融科技- B端SaaS-金融法务科技），博白某小学支教，3次国奖获得者，雅思自学7.5 DIY申请布里斯托大学offer获得者"
   },
   {
@@ -271,7 +271,7 @@ export const members: Member[] = [
     "mbti": "ENTJ",
     "hobbies": ["music", "dance"],
     "avatar": "/christmas/image_25.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "Frank is the chief of staff at Troveo.ai, a US-based startup under the 776 fund. He's born and raised in Shenzhen and now lives in Chicago / LA"
   },
   {
@@ -282,7 +282,7 @@ export const members: Member[] = [
     "zodiac": "处女座",
     "hobbies": ["读小说"],
     "avatar": "/christmas/image_26.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "来自印度尼西亚，香港中文大学（深圳）数据科学专业26届毕业的。毕业之后我希望能留在深圳继续发展"
   },
   {
@@ -292,7 +292,7 @@ export const members: Member[] = [
     "mbti": "ENFP",
     "hobbies": ["戏剧表演", "现代舞"],
     "avatar": "/christmas/image_27.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "潜意识心理咨询师，详甜睿学创始人，剑桥人类学杰出硕士"
   },
   {
@@ -313,7 +313,7 @@ export const members: Member[] = [
     "zodiac": "金牛",
     "hobbies": ["看海", "制作个性AI礼物"],
     "avatar": "/christmas/image_29.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "香港人、經營跳舞學校及咖啡店"
   },
   {
@@ -323,7 +323,7 @@ export const members: Member[] = [
     "mbti": "ENFP",
     "hobbies": ["旅游", "电影", "建筑相关"],
     "avatar": "/christmas/image_30.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "AI行业市场岗 希望结识更多同行好友多交流"
   },
   {
@@ -334,7 +334,7 @@ export const members: Member[] = [
     "zodiac": "巨蟹座",
     "hobbies": ["德州扑克"],
     "avatar": "/christmas/image_31.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "前罗永浩战略现ai硬件创业者"
   },
   {
@@ -344,7 +344,7 @@ export const members: Member[] = [
     "zodiac": "水瓶座",
     "hobbies": ["美食", "旅游"],
     "avatar": "/christmas/image_32.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "香港工作，创业公司合伙人"
   },
   {
@@ -354,7 +354,7 @@ export const members: Member[] = [
     "zodiac": "双子",
     "hobbies": ["打坐"],
     "avatar": "/christmas/image_33.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "专注线下消费智能化升级、共享经济第一批创业者、打造小铁共享寄存、小铁自助台球室"
   },
   {
@@ -365,7 +365,7 @@ export const members: Member[] = [
     "zodiac": "天蝎",
     "hobbies": ["研究玄学"],
     "avatar": "/christmas/image_34.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "早期互联网猎头，后转型web3自由猎头，目前在一家IOT+AI的科技公司做总助"
   },
   {
@@ -376,7 +376,7 @@ export const members: Member[] = [
     "zodiac": "水瓶座",
     "hobbies": ["读书"],
     "avatar": "/christmas/image_35.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "烁途科技联合创始人，AI教育，致力于帮助青少年用AI解决真实问题"
   },
   {
@@ -387,7 +387,7 @@ export const members: Member[] = [
     "zodiac": "双子",
     "hobbies": ["玩具", "制作个性AI礼物"],
     "avatar": "/christmas/image_36.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "美国ai硬件创业中，前高通工程师，目前招聘寻找大厂做agent架构的专家和在校实习生"
   },
   {
@@ -396,7 +396,7 @@ export const members: Member[] = [
     "phone": "13038892672",
     "mbti": "ENFP",
     "avatar": "/christmas/image_37.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "丝绸之路"
   },
   {
@@ -407,7 +407,7 @@ export const members: Member[] = [
     "zodiac": "處女座",
     "hobbies": ["足球", "音樂"],
     "avatar": "/christmas/image_38.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg"
+    "bgImage": "/mbti/mbti-green.jpg"
   },
   {
     "idx": "39",
@@ -417,7 +417,7 @@ export const members: Member[] = [
     "zodiac": "白羊座",
     "hobbies": ["撸猫", "睡大觉", "电影"],
     "avatar": "/christmas/image_39.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "自动化工科女｜现在做香港业务与资产配置"
   },
   {
@@ -428,7 +428,7 @@ export const members: Member[] = [
     "zodiac": "獅子座",
     "hobbies": ["公共交通", "巴士", "飛機"],
     "avatar": "/christmas/image_40.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "從事金融法規和商業地產中介，熱愛旅行，喜歡到處打卡，社交能力一般"
   },
   {
@@ -438,7 +438,7 @@ export const members: Member[] = [
     "zodiac": "水瓶座",
     "hobbies": ["看蜡笔小新"],
     "avatar": "/christmas/image_41.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "线上互相了解，i人一个"
   },
   {
@@ -448,7 +448,7 @@ export const members: Member[] = [
     "mbti": "INTP",
     "hobbies": ["coding", "AI"],
     "avatar": "/christmas/image_42.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "来新璐，奇绩 f24， shareAI 创始人，b 站 up 飞鸟白菜。之前在百度飞桨和腾讯混元待过，AI 跟全栈都搞过，公司现在主要做第二大脑，资料知识库+ AI agent 搜索"
   },
   {
@@ -458,7 +458,7 @@ export const members: Member[] = [
     "zodiac": "天蝎座",
     "hobbies": ["AI", "健身", "旅游", "摄影"],
     "avatar": "/christmas/image_43.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "AI实践者&倡导者 AI社区主理人 AI产品经理"
   },
   {
@@ -479,7 +479,7 @@ export const members: Member[] = [
     "zodiac": "天蝎座",
     "hobbies": ["科幻", "乐器", "温泉"],
     "avatar": "/christmas/image_45.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "I know、I know、I know。多个智能产品和品牌的策划人。只做世界上没有的新品类。（theone、音乐密码、aeroband、折叠琴、大屏琴、智能书桌、猫女机器人等）"
   },
   {
@@ -489,7 +489,7 @@ export const members: Member[] = [
     "zodiac": "狮子座",
     "hobbies": ["搞钱", "花钱"],
     "avatar": "/christmas/image_46.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "幼教界泥石流，一个有料有趣的幼教人"
   },
   {
@@ -499,7 +499,7 @@ export const members: Member[] = [
     "zodiac": "金牛座",
     "hobbies": ["旅行"],
     "avatar": "/christmas/image_47.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg"
+    "bgImage": "/mbti/mbti-yellow.jpg"
   },
   {
     "idx": "48",
@@ -509,7 +509,7 @@ export const members: Member[] = [
     "zodiac": "巨蟹",
     "hobbies": ["画画", "听歌"],
     "avatar": "/christmas/image_48.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "23年毕业于意大利Brera美院，专业是舞台设计。目前从事亚马逊电商工作，同时对ai领域充满兴趣，很期待通过这次活动向大家学习"
   },
   {
@@ -519,7 +519,7 @@ export const members: Member[] = [
     "zodiac": "金牛",
     "hobbies": ["旅行", "吃喝玩"],
     "avatar": "/christmas/image_49.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "卡皮巴拉性格、曾經帥過的派大星"
   },
   {
@@ -540,7 +540,7 @@ export const members: Member[] = [
     "zodiac": "双子",
     "hobbies": ["爬山"],
     "avatar": "/christmas/image_51.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "陈铭，目前从事机器人行业的工作，希望跟大家成为朋友"
   },
   {
@@ -561,7 +561,7 @@ export const members: Member[] = [
     "zodiac": "巨蟹",
     "hobbies": ["户外", "养生", "睡觉"],
     "avatar": "/christmas/image_53.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "我是南方小土豆，很怕冷，但有活动聚会的热情，喜欢各类运动，但水平业余，现从事金融行业数据相关工作，希望多认识各行业友人"
   },
   {
@@ -571,7 +571,7 @@ export const members: Member[] = [
     "zodiac": "天秤",
     "hobbies": ["美食", "运动", "看展"],
     "avatar": "/christmas/image_54.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "喜欢交朋友和探索新事物的吃货一枚，间歇性运动经常性收藏健身视频～从事金融模型相关工作，希望多拥抱AI，与各行业友人多交流"
   },
   {
@@ -581,7 +581,7 @@ export const members: Member[] = [
     "mbti": "INTJ转ENTP",
     "hobbies": ["冲浪"],
     "avatar": "/christmas/image_55.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "欧铭龙，周周黑客松主理人，oneAPIdoc.com co-founder"
   },
   {
@@ -592,7 +592,7 @@ export const members: Member[] = [
     "zodiac": "天秤座",
     "hobbies": ["读书", "音乐", "运动"],
     "avatar": "/christmas/image_56.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "过往的职业经历包括科技创投媒体记者&投资机构&创业公司海外业务负责人 长期关注出海&海外PR及品牌"
   },
   {
@@ -600,7 +600,7 @@ export const members: Member[] = [
     "name": "Wjll",
     "phone": "none",
     "avatar": "/christmas/image_57.jpeg",
-    "bgImage": "/mbti/mbti-blue.jpg"
+    "bgImage": "/mbti/mbti-yellow.jpg"
   },
   {
     "idx": "58",
@@ -621,7 +621,7 @@ export const members: Member[] = [
     "zodiac": "处女座",
     "hobbies": ["旅行"],
     "avatar": "/christmas/image_59.jpeg",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "剪映AI工具产品体验设计师，兼JustAI产品设计负责人"
   },
   {
@@ -631,7 +631,7 @@ export const members: Member[] = [
     "mbti": "INTP",
     "hobbies": ["mmorpg"],
     "avatar": "/christmas/image_60.png",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-purple.jpg",
     "intro": "互联网公司商务，对ai行业感兴趣，目前在做模型微调合作，智能体项目落地"
   },
   {
@@ -652,7 +652,7 @@ export const members: Member[] = [
     "mbti": "ENFP",
     "hobbies": ["跳舞", "看书"],
     "avatar": "/christmas/image_62.png",
-    "bgImage": "/mbti/mbti-yellow.jpg",
+    "bgImage": "/mbti/mbti-green.jpg",
     "intro": "后大厂正能量创业女孩"
   },
   {
@@ -660,7 +660,7 @@ export const members: Member[] = [
     "name": "饼干",
     "phone": "none",
     "avatar": "/christmas/image_63.png",
-    "bgImage": "/mbti/mbti-blue.jpg",
+    "bgImage": "/mbti/mbti-yellow.jpg",
     "intro": "饼干"
   }
 ];
